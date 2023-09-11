a39cf6bb89f298ed9471306d278633406aebc523
AsylbaevMM <mister.asylbaeff@gmail.com>
2023-09-11 16:39:50 +0300

    change DB to PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from .ignorefile import SECRET_KEY
+from .ignorefile import SECRET_KEY, DATABASEPASS
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,8 +84,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': DATABASEPASS,
     }
 }
 
