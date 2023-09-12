391cada233c26dff2f2817a274b575ced1a5869c
AsylbaevMM <mister.asylbaeff@gmail.com>
2023-09-12 22:04:22 +0300

    create profile update form

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,20 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def profile(request):
-    return render(request, 'registration/profile.html')
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users-profile')
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(request, 'registration/profile.html', {'user_form': user_form, 'profile_form': profile_form})
 
 
 class SignUpView(generic.CreateView):
