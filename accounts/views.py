2d0088d756e2d1a7fc2a70a44cf7c4a0aefe72f3
AsylbaevMM <mister.asylbaeff@gmail.com>
2023-09-12 17:49:14 +0300

    created sessions

--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import SignUpForm
+from .forms import SignUpForm, LoginForm
 from django.contrib import messages
+from django.contrib.auth.views import LoginView
+
 
 class SignUpView(generic.CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy("login")
     initial = None  # принимает {'key': 'value'}
-    template_name = "registration/signup.html"
+    template_name = 'registration/signup.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # перенаправит на домашнюю страницу, если пользователь попытается получить доступ к странице регистрации после авторизации
+        if request.user.is_authenticated:
+            return redirect(to='/')
+
+        return super(SignUpView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         form = self.form_class(initial=self.initial)
@@ -20,7 +29,24 @@ class SignUpView(generic.CreateView):
 
         if form.is_valid():
             form.save()
+
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
-            return redirect(to='/')
+
+            return redirect(to='login') # редирект на страницу логина после регистрации
+
         return render(request, self.template_name, {'form': form})
+
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+
+    def form_valid(self, form):
+        remember_me = form.cleaned_data.get('remember_me')
+
+        if not remember_me:
+            # Устанавливаем время истечения сеанса равным 0 секундам. Сеанс закроется при закрытии браузера.
+            self.request.session.set_expiry(0)
+            self.request.session.modified = True
+
+        # Если remember_me == True время сеанса можно установить в "SESSION_COOKIE_AGE" файла settings.py
+        return super(CustomLoginView, self).form_valid(form)
