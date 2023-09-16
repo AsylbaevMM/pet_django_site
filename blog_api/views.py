199a4feb44872e6f5d96014d6db0509cd3929dbb
AsylbaevMM <mister.asylbaeff@gmail.com>
2023-09-16 11:03:28 +0300

    create pagination for API view

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,13 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from blog.models import Post
 from .serializers import PostSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 8
+    page_size_query_param = 'page_size'
+    max_page_size = 10
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['author']
+    search_fields = ['body', 'author__username', 'author__profile__bio']
+    ordering_fields = ['body', 'author__username']
+    ordering = ['body']
+    pagination_class = StandardResultsSetPagination
 
     # def get_queryset(self):
     #     user = self.request.user
@@ -26,3 +36,9 @@ class UserPostList(generics.ListAPIView):
     def get_queryset(self):
         user = self.kwargs['username']
         return Post.objects.filter(author=user)
+
+class CustomSearchFilter(filters.SearchFilter):
+    def get_search_fields(self, view, request):
+        if request.query_params.get('title_only'):
+            return ['title']
+        return super().get_search_fields(view, request)
